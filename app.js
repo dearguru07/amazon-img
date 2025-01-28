05201197a0908f66f25b5fe77d4e96809cd98764
dearguru07 <pangalaguruyadav101@gmail.com>
2025-01-28 17:10:11 +0530

    new

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
-// let count = 0;
+// Pure functionS---------
 
+// function add(a, b) {
+//   return a + b;
+// }
+
+// // Example usage:
+// console.log(add(2, 3)); // Always returns 5
+// console.log(add(2, 3)); // Always returns 5
+
+
+// // Impure Functions-----------
+
+// let count = 0;
 // function increment() {
 //   count++;
 //   return count;
 // }
 
-// // Example usage:
 // console.log(increment()); // Output depends on the current state of `count`
 // console.log(increment()); // Output depends on the current state of `count`
 
 
+// let sol=hello()
+// function hello(){
+//   var c=205;
+//   console.log(c)
+// }
 
-// function add(a, b) {
-//     return a + b;
-//   }
-  
-//   // Example usage:
-//   console.log(add(2, 3)); // Always returns 5
-//   console.log(add(2, 3)); // Always returns 5
-
-  
-let count = 0;
-
-function increment() {
-  count++;
-  return count;
+let z=25
+a()
+function a(){
+  console.log(z)
+  var c=20
+  console.log('hello world....')
 }
-
-// Example usage:
-console.log(increment()); // Output depends on the current state of `count`
-console.log(increment()); // Output depends on the current state of `count`
