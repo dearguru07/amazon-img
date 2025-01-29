bc5fc5f228f4224d4267ea4f761df93b323bc77b
dearguru07 <pangalaguruyadav101@gmail.com>
2025-01-29 10:34:59 +0530

    done

--- app.py
+++ app.py
@@ -74,3 +74,18 @@ if is_armstrong(num):
 else:
     print(f"{num} is not an Armstrong number.")
 
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# Example Usage
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
